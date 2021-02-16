8c40d95b969a4e1472dc0c88e739867b1c2aca92
Aaron Eline <aeline@umd.edu>
2021-02-15 20:09:55 -0800

    Improved setup script

--- setuproot.sh
+++ setuproot.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+echo "Compiling containers!"
+cargo b
 echo "Creating the root file system!"
 mkdir root
 mkdir rootfs
 cd rootfs
+echo "Downloading alpine linux"
 wget http://nl.alpinelinux.org/alpine/v3.7/releases/x86_64/alpine-minirootfs-3.7.0-x86_64.tar.gz -O fs.tar.gz
 tar -xzvf fs.tar.gz
 rm fs.tar.gz
